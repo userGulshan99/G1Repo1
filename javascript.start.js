53ab7a23c94e87acc9387eececbdbe5facf84547
userGulshan99 <147707258+userGulshan99@users.noreply.github.com>
2024-04-20 23:11:26 +0530

    added arrray

--- javascript.start.js
+++ javascript.start.js
@@ -1,4 +1,7 @@
 
-welcome to starting point of learning of github.
+// welcome to starting point of learning of github.
 
   console.log("Hello World!");
+
+// lets add something new
+let arr=[1,2,3,4];
