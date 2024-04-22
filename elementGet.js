36bb237763abf4eac9f7f4ef1c0411adfbf07556
userGulshan99 <bankargulshan@gmail.com>
2024-04-22 22:09:39 +0530

    element by TagName

--- elementGet.js
+++ elementGet.js
@@ -1,21 +1,7 @@
 
-const h1=document.getElementById("main-heading");
-h1.innerHTML="Fruit World";
-h1.style.color="orange";
+const list= document.getElementsByTagName('li');
+list[4].style.color="blue";
 
-const idselect=document.getElementById("header");
-idselect.style.backgroundColor="green";
-idselect.style.borderBottom="3px solid orange"
-
-const h2select=document.getElementById("basket-heading");
-h2select.style.color="green";
-
-const thanks = document.getElementById("thanks");
-thanks.innerHTML="<p>Please visit us again</p>"
-
-const fruit= document.getElementsByClassName('fruit');
-fruit[2].style.backgroundColor="orange"
-
-for(let i=0;i<fruit.length;i++){
-    fruit[i].style.fontWeight='bold';
+for(let i=0;i<list.length;i++){
+    list[i].style.fontStyle="italic";
 }
