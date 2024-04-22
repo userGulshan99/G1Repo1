a72733fd6f1ac2cef4405fd9f59d1e82fead7bc3
userGulshan99 <bankargulshan@gmail.com>
2024-04-22 18:43:00 +0530

    DOM manipulation by getElementById

---
+++ elementGet.js
@@ -0,0 +1,15 @@
+
+const h1=document.getElementById("main-heading");
+h1.innerHTML="Fruit World";
+h1.style.color="orange";
+
+const idselect=document.getElementById("header");
+idselect.style.backgroundColor="green";
+idselect.style.borderBottom="3px solid orange"
+
+const h2select=document.getElementById("basket-heading");
+h2select.style.color="green";
+
+const thanks = document.getElementById("thanks");
+thanks.innerHTML="<p>Please visit us again</p>"
+
