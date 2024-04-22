2cb28ed86ad8bc9d0f56c4e51d1e6c4f13926a15
userGulshan99 <bankargulshan@gmail.com>
2024-04-22 22:54:31 +0530

    querySelector & querySelectorAll

--- elementGet.js
+++ elementGet.js
@@ -1,7 +1,30 @@
 
-const list= document.getElementsByTagName('li');
-list[4].style.color="blue";
+const mainheading=document.querySelector('#main-heading');
+mainheading.style.textAlign="center";
 
-for(let i=0;i<list.length;i++){
-    list[i].style.fontStyle="italic";
+const fruits=document.querySelector(".fruits");
+fruits.style.backgroundColor="gray";
+fruits.style.padding="30px";
+fruits.style.margin="30px";
+fruits.style.width="50%";
+fruits.style.borderRadius="5px";
+fruits.style.listStyleType="none";
+
+const basketheading = document.querySelector('#basket-heading');
+basketheading.style.marginLeft="30px";
+basketheading.style.color="brown";
+
+const fruitItems=document.querySelectorAll(".fruit");
+
+for(let i=0;i<fruitItems.length;i++){
+    fruitItems[i].style.padding='10px';
+    fruitItems[i].style.margin='10px';
+    fruitItems[i].style.borderRadius='5px';
+
+    if(i%2!=0){
+        fruitItems[i].style.backgroundColor='brown';
+        fruitItems[i].style.color='white';
+    }else{
+        fruitItems[i].style.backgroundColor='white';
+    }
 }
