9526a0ebc12fd38d6ff62db09e5f70fb198fde2f
userGulshan99 <bankargulshan@gmail.com>
2024-04-24 17:03:14 +0530

    Add fruits with description and search fruits by using fruit name or description

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
 
-let sel=document.querySelectorAll('li');
 
-for(let i=0;i<sel.length;i++){
-    let button=document.createElement('button');
-    let btnText=document.createTextNode('edit');
-    button.className='edit-btn';
-    button.appendChild(btnText);
-    sel[i].appendChild(button);
-}
 
-let form=document.querySelector('form');
-let list=document.getElementsByClassName('fruits');
+// Add input element inside form, before button, to take fruit description
+const form=document.querySelector('form');
+const description_button=document.createElement('input');
+description_button.id='description';
+let lastnode=form.lastElementChild;
+form.insertBefore(description_button,lastnode);
+
+// Show the fruit description in italics on the next line
+const list=document.querySelector('.fruits');
 
 form.addEventListener('submit',function (event) {
     event.preventDefault();
-    let fv=document.getElementById("fruit-to-add");
+    // created list element
     let ele=document.createElement("li");
-    let text=document.createTextNode(fv.value);
-    ele.appendChild(text);
     ele.className="fruit";
-    
-    let bttn=document.createElement('button');
-    bttn.type="submit";
-    bttn.className='delete-btn';
-    let btnval=document.createTextNode("x");
-    bttn.appendChild(btnval);
 
-    let editbtn=document.createElement('button');
-    editbtn.className='edit-btn'
-    let editText=document.createTextNode('edit');
-    editbtn.appendChild(editText);
-    ele.appendChild(bttn);
-    ele.appendChild(editbtn);
-    list[0].appendChild(ele);
-    console.log(list);
+    // fruit name appending to the li child
+    let fruit_input=document.getElementById("fruit-to-add");
+    let text=document.createTextNode(fruit_input.value);
+    ele.appendChild(text);
+    
+    // created para for description
+    let parafordescr=document.createElement('p');
+    let descr=document.getElementById('description');
+    let descrText=document.createTextNode(descr.value);
+    parafordescr.appendChild(descrText);
+    parafordescr.style.fontStyle='italic';
+    ele.appendChild(parafordescr);
+    list.appendChild(ele);
 })
 
-list[0].addEventListener('click',function a(event){
-    if(event.target.classList.contains('delete-btn')){
-        let parentEle=event.target.parentElement;
-        list[0].removeChild(parentEle);
+let search=document.getElementById('filter');
+
+search.addEventListener('keyup',function(event){
+    let searching_value=search.value.toLowerCase();
+
+    const liEle = document.getElementsByClassName('fruit')
+
+    for(let i=0;i<liEle.length;i++){
+        let list_value=liEle[i].innerText.toLowerCase();
+
+        if(list_value.includes(searching_value)){
+            liEle[i].style.display="flex";
+        }else{
+            liEle[i].style.display="none";
+        }
     }
 })
-
-
