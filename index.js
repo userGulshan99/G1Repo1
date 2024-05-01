469c1654bb5275a68daf34ac30b371cfd8f50465
userGulshan99 <bankargulshan@gmail.com>
2024-05-01 08:28:55 +0530

    added delete button that will delete list element and also delete data from storage

--- index.js
+++ index.js
@@ -12,8 +12,19 @@ function handleFormSubmit(event){
     
     let li=document.createElement('li');
     let liText=document.createTextNode(a);
+    let btn=document.createElement('input');
+    btn.type='button';
+    btn.value='delete';
+  
     li.appendChild(liText);
+    li.appendChild(btn);
     ul[0].appendChild(li);
-localStorage.setItem( event.target.email.value  , a);
- 
+    
+    localStorage.setItem(event.target.email.value , a);
+
+    btn.addEventListener('click', function del(event){
+        localStorage.removeItem(JSON.parse(event.target.parentElement.textContent).email)
+        event.target.parentElement.remove();
+    })
+
 }
